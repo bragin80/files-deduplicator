d7c164aef26ebd97eb5794015266e1ff17134079
bragin <bragincoop@gmail.com>
2025-09-16 19:57:02 +0300

    task commit

--- src/main/java/by/bragin/web/controller/EchoController.java
+++ src/main/java/by/bragin/web/controller/EchoController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class EchoController {
 
     @GetMapping
-    public ResponseEntity<String> echo() {
-        return new ResponseEntity<>("Hello world!", HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public String echo() {
+        return "Hello World!";
     }
 }
