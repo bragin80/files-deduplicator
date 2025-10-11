2c8083461b7e7ee44877f8ca4ee190fd6b34b52d
aleksandr.shevtsov <developer.alex.shevtsov80@gmail.com>
2025-10-11 18:08:01 +0300

    #41 - file table changelog added + tests configuration

--- fs-smart-utils/fs-accessor-service/src/test/java/by/bragin/accessor/AccessorServiceApplicationTest.java
+++ fs-smart-utils/fs-accessor-service/src/test/java/by/bragin/accessor/AccessorServiceApplicationTest.java
@@ -1,7 +1,10 @@
 package by.bragin.accessor;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 public class AccessorServiceApplicationTest {
