6ca5b788521f556958f36c8a41947adaf1391d3c
aleksandr.shevtsov <developer.alex.shevtsov80@gmail.com>
2025-10-11 13:20:52 +0300

    repository example

---
+++ fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/persistence/entity/File.java
@@ -0,0 +1,10 @@
+package by.bragin.accessor.persistence.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+// @Entity
+public class File extends BaseEntity {
+}
---
+++ fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/persistence/repository/FileRepository.java
@@ -0,0 +1,8 @@
+package by.bragin.accessor.persistence.repository;
+
+import by.bragin.accessor.persistence.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface FileRepository { //extends JpaRepository<File, Long> {
+}
