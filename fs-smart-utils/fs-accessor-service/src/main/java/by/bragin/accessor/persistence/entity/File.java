93ef7e29425a0e70419f6cff62da5c632a77db4d
bragin80 <bragincoop@gmail.com>
2025-10-11 15:44:30 +0300

    #32 file-db task commit (#43)

--- fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/persistence/entity/File.java
+++ fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/persistence/entity/File.java
@@ -1,11 +1,24 @@
 package by.bragin.accessor.persistence.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 // @Entity
 public class File extends BaseEntity {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String type;
+    private Long size;
+    private String md5;
+    private LocalDateTime creationDate;
+    private LocalDateTime lastModified;
 }
