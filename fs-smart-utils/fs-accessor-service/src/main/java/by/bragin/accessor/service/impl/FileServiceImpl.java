ddba7f943f7d0fa2c65326b69c29ca30ec538404
aleksandr.shevtsov <developer.alex.shevtsov80@gmail.com>
2025-10-12 09:40:54 +0300

    #37 accessopr service initial

--- fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/service/impl/FileServiceImpl.java
+++ fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/service/impl/FileServiceImpl.java
@@ -4,9 +4,11 @@ import by.bragin.accessor.persistence.entity.File;
 import by.bragin.accessor.persistence.repository.FileRepository;
 import by.bragin.accessor.service.FileService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class FileServiceImpl implements FileService {
