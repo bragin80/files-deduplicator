bfe09545f6b947d158b9dfeae4db16de9c526fee
aleksandr.shevtsov <developer.alex.shevtsov80@gmail.com>
2025-10-12 09:56:36 +0300

    #37 tests review

--- fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/service/impl/FileServiceImpl.java
+++ fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/service/impl/FileServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class FileServiceImpl implements FileService {
     private final FileRepository fileRepository;
