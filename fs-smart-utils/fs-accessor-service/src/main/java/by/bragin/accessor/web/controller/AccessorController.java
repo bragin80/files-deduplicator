3607ccefa3d96b0f01808263d7629cfbf71dc44e
aleksandr.shevtsov <developer.alex.shevtsov80@gmail.com>
2025-10-26 14:15:27 +0300

    #48 tests fix

--- fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/web/controller/AccessorController.java
+++ fs-smart-utils/fs-accessor-service/src/main/java/by/bragin/accessor/web/controller/AccessorController.java
@@ -11,7 +11,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class AccessorController implements AccessorContract {
-
     private final FilesScanService scanService;
 
     @Override
--- fs-smart-utils/fs-accessor-service/src/test/java/by/bragin/accessor/configs/TestConfig.java
+++ fs-smart-utils/fs-accessor-service/src/test/java/by/bragin/accessor/configs/TestConfig.java
@@ -1,8 +1,20 @@
 package by.bragin.accessor.configs;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@AutoConfigureMockMvc
+@TestConfiguration
 public class TestConfig {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Bean
+    public MockMvc mockMvc(){
+        return MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 }
